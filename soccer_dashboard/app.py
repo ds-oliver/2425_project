662b5e4630fd610984ace2726d7fcdb8ab203d47
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2024-06-12 14:10:01 -0700

    Refactor add_badges function and update column order in app.py

--- soccer_dashboard/app.py
+++ soccer_dashboard/app.py
@@ -492,17 +492,14 @@ def add_badges(df, badges, playerwise=True):
     # Determine the order of the columns
     if playerwise:
         if "assist_player" in df_badges.columns:
-            columns_order = ["img", "assist_player", "Open Play xG"] + [
+            columns_order = ["img", "assist_player", "team", "Open Play xG"] + [
                 col
                 for col in df_badges.columns
-                if col not in ["img", "assist_player", "team", "Open Play xG"]
+                if col
+                not in ["img", "assist_player", "team", "Open Play xG"]
             ]
-            if "position" in df_badges.columns:
-                columns_order.insert(
-                    3, "position"
-                )  # Insert position column if it exists
         else:
-            columns_order = ["img", "player", "position", "Open Play xG"] + [
+            columns_order = ["img", "player", "position", "team", "Open Play xG"] + [
                 col
                 for col in df_badges.columns
                 if col not in ["img", "player", "position", "team", "Open Play xG"]
